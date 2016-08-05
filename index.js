5acafa350af32081dd0ca398d062bf6f8fed8153
Peng.Li <peng.li@rea-group.com>
2016-08-05 16:38:56 +0800

    get param example

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
   res.send('Hello, world!');
 });
 
+app.get('/hello/:name', function(req, res) {
+  res.send('Hello, ' + req.params.name);
+})
+
 app.listen(3000, function () {
   console.log('Server listening at http://localhost:3000');
 });
